f507d848e31bb02cd3f6707a1ba339838d29d12d
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-06-10 18:10:47 -0500

    Updates object names

--- server.js
+++ server.js
@@ -5,208 +5,208 @@ const PORT = 8000
 
 app.use(cors())
 
-const books = {
-    "chato's kitchen": {
+const years = {
+    "1996": {
         'bookTitle': "Chato's Kitchen",
         'authorName': "Gary Soto",
         'illustratorName': "Susan Guevara"
     },
-    "the farolitos of christmas": {
+    "1996": {
         'bookTitle': "The Farolitos of Christmas",
         'authorName': 'Rudolfo Anaya',
         'illustratorName': 'Edward Gonzales' 
     },
-    "in my family/en mi familia": {
+    "1997": {
         'bookTitle':"In My Family/En Mi Familia",
         'authorName': 'Carmen Lomas Garza',
         'illustratorName': 'Carmen Lomas Garza'
     },
-    "tomás and the library lady": {
+    "1998": {
         'bookTitle': "Tomás and the Library Lady",
         'authorName':'Pat Mora',
         'illustratorName': 'Raúl Colón'
     },
-    "the three pigs/los tres cerdos: nacho, tito, and miguel": {
+    "1999": {
         'bookTitle': "The Three Pigs/Los Tres Cerdos: Nacho, Tito, and Miguel",
         'authorName': 'Bobbi Salinas',
         'illustratorName': 'Bobbi Salinas'
     },
-    "my land sings: stories of the rio grande": {
+    "2000": {
         'bookTitle': "My Land Sings: Stories of the Rio Grande",
         'authorName': 'Rudolfo Anaya',
         'illustratorName': 'Amy Córdova'
     },
-    "my very own room/mi propio cuartito": {
+    "2001": {
         'bookTitle': "My Very Own Room/Mi propio cuartito",
         'authorName': 'Amada Irma Pérez',
         'illustratorName': 'Maya Christina Gonzalez'
     },
-    "breaking through": {
+    "2002": {
         'bookTitle': "Breaking Through",
         'authorName': 'Francisco Jiménez',
         'illustratorName':'N/A'
     },
-    "a library for juana: the world of sor juana": {
+    "2003": {
         'bookTitle': "A Library for Juana: The World of Sor Juana",
         'authorName': 'Pat Mora',
         'illustratorName': 'Beatriz Vidal'
     },
-    "just a minute: a trickster tale and counting book": {
+    "2004": {
         'bookTitle': "Just a Minute: A Trickster Tale and Counting Book",
         'authorName': ' Yuyi Morales',
         'illustratorName': 'Yuyi Morales'
     },
-    "becoming naomi león": {
+    "2005": {
         'bookTitle': "Becoming Naomi León",
         'authorName': 'Pam Muoñz Ryan',
         'illustratorName': 'N/A'
     },
-    "josé! born to dance": {
+    "2006": {
         'bookTitle': "José! Born to Dance",
         'authorName': 'Susanna Reich',
         'illustratorName': 'Raúl Colón'
     },
-    "downtown boy": {
+    "2007": {
         'bookTitle': "Downtown Boy",
         'authorName': 'Juan Felipe Herrera',
         'illustratorName':'N/A'
     },
-    "los gatos black on halloween": {
+    "2008": {
         'bookTitle': "Los Gatos Black on Halloween",
         'authorName': 'Marisa Montes',
         'illustratorName': 'Yuyi Morales'
     },
-    "he forgot to say goodbye": {
+    "2009": {
         'bookTitle': "He Forgot To Say Goodbye",
         'authorName': 'Benjamin Alire Sáenz',
         'illustratorName': 'N/A'
     },
-    "the holy tortilla and a pot of beans": {
+    "2009": {
         'bookTitle': "The Holy Tortilla and a Pot of Beans",
         'authorName': 'Carmen Tafolla',
         'illustratorName': 'N/A'
     },
-    "what can you do with a paleta?": {
+    "2010": {
         'bookTitle': "What Can You Do With a Paleta?",
         'authorName': 'Carmen Tafolla',
         'illustratorName':'Magaly Morales'
     },
-    "bait": {
+    "2011": {
         'bookTitle': "Bait",
         'authorName': 'Alex Sanchez',
         'illustratorName': 'N/A'
     },
-    "diego rivera: his world and ours": {
+    "2012": {
         'bookTitle': "Diego Rivera: His World and Ours",
         'authorName': 'Duncan Tonatiuh',
         'illustratorName': 'Duncan Tonatiuh'
     },
-    "sylvia and aki": {
+    "2012": {
         'bookTitle': "Sylvia and Aki" ,
         'authorName': ' Winifred Conkling',
         'illustratorName': 'N/A'
     },
-    "under the mesquite": {
+    "2013": {
         'bookTitle': "Under the Mesquite",
         'authorName': 'Guadalupe Garcia McCall',
         'illustratorName': 'N/A'
     },
-    "diego rivera: an artist for the people": {
+    "2014": {
         'bookTitle': "Diego Rivera: An Artist for the People",
         'authorName': 'Susan Goldman Rubin',
         'illustratorName': 'Susan Goldman Rubin'
     },
-    "pancho rabbit and the coyote: a migrant's tale": {
+    "2014": {
         'bookTitle': "Pancho Rabbit and the Coyote: A Migrant's Tale",
         'authorName': 'Duncan Tonatiuh',
         'illustratorName': 'Duncan Tonatiuh'
     },
-    "gabi: a girl in pieces": {
+    "2015": {
         'bookTitle': "Gabi: A Girl in Pieces",
         'authorName': 'Isabel Quintero',
         'illustratorName': 'N/A'
     },
-    "separate is never equal": {
+    "2015": {
         'bookTitle': "Separate is Never Equal",
         'authorName': 'Duncan Tonatiuh',
         'illustratorName': 'Duncan Tonatiuh'
     },
-    "out of darkness": {
+    "2016": {
         'bookTitle': "Out of Darkness",
         'authorName': 'Ashley Hope Pérez',
         'illustratorName': 'N/A'
     },
-    "funny bones: posada and his day of the dead calaveras": {
+    "2016": {
         'bookTitle': "Funny Bones: Posada and His Day of the Dead Calaveras",
         'authorName': 'Duncan Tonatiuh',
         'illustratorName': 'Duncan Tonatiuh'
     },
-    "the memory of light": {
+    "2017": {
         'bookTitle': "The Memory of Light",
         'authorName': 'Francisco X. Stork',
         'illustratorName': 'N/A'
     },
-    "maybe something beautiful: how art transformed a neighborhood": {
+    "2017": {
         'bookTitle': "Maybe Something Beautiful: How Art Transformed a Neighborhood",
         'authorName': "Isabel Campoy & Theresa Howell",
         'illustratorName': 'Rafael López'
     },
-    "i am not your perfect mexican daughter": {
+    "2018": {
         'bookTitle': "I Am Not Your Perfect Mexican Daughter",
         'authorName': 'Erika L. Sánchez',
         'illustratorName': 'N/A'
     },
-    "the first rule of punk": {
+    "2018": {
         'bookTitle': "The First Rule of Punk",
         'authorName': 'Celia C. Pérez',
         'illustratorName': 'N/A'
     },
-    "all around us": {
+    "2018": {
         'bookTitle': "All Around Us" ,
         'authorName': "Xelena González",
         'illustratorName': 'Adriana M. Garcia'
     },
-    "they call me güero, a border kid's poems": {
+    "2019": {
         'bookTitle': "They Call me Güero, A Border Kid's Poems",
         'authorName': "David Bowles",
         'illustratorName': "N/A"
     },
-    "dreamers": {
+    "2019": {
         'bookTitle': "Dreamers" ,
         'authorName': "Yuyi Morales",
         'illustratorName': "Yuyi Morales"
     },
-    "my papi has a motorcycle": {
+    "2020": {
         'bookTitle': "My Papi Has A Motorcycle",
         'authorName':"Isabel Quintero",
         'illustratorName': "Zeke Peña"
     },
-    "dreaming with mariposas": {
+    "2021": {
         'bookTitle': "Dreaming with Mariposas",
         'authorName': "Sonia Gutiérrez",
         'illustratorName': 'N/A'
     },
-    "the spirit of chicano park":{
+    "2021":{
         'bookTitle': "The Spirit of Chicano Park/El espíritu del Parque Chicano",
         'authorName': 'Beatrice Zamora',
         'illustratorName': 'Maira Meza'
     },
-    "feathered serpent and the five suns: a mesoamerican creation myth": {
+    "2021": {
         'bookTitle': "Feathered Serpent and the Five Suns: A Mesoamerican Creation Myth",
         'authorName': 'Duncan Tonatiuh',
         'illustratorName': 'N/A'
     },
-    "bright star": {
+    "2022": {
         'bookTitle': "Bright Star",
         'authorName': 'Yuyi Morales',
         'illustratorName': 'Yuyi Morales'
     },
-    "my two border towns": {
+    "2022": {
         'bookTitle': "My Two Border Towns",
         'authorName': 'David Bowles',
         'illustratorName': 'Erika Meza'
     },
-    "indivisible": {
+    "2022": {
         'bookTitle': "Indivisible",
         'authorName': 'Daniel Aleman',
         'illustratorName': 'N/A'
@@ -224,11 +224,11 @@ app.get('/', (request, response) => {
 
 app.get('/api/:title', (request, response) => {
     const bookTitles = request.params.title.toLowerCase()
-    console.log(books[bookTitles])
-    if( books[bookTitles] ){
-        response.json(books[bookTitles])
+    console.log(years[bookTitles])
+    if( years[bookTitles] ){
+        response.json(years[bookTitles])
     }else{
-        response.json(books['unknown'])
+        response.json(years['unknown'])
     }
 })
 
